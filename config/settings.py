df5fcabc412cdb020a9f0b2a47127a5bc2ff6c84
rohan2005040-rgb <rohan2005040@gmail.com>
2026-08-15 18:48:12 +0600

    Add production error logging

--- config/settings.py
+++ config/settings.py
@@ -199,3 +199,22 @@ LOGOUT_REDIRECT_URL = "/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
